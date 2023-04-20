f761f0350d84401c3836f6bd5d40172d26fa56b7
SadS4ndWiCh <caiocamargo007@gmail.com>
2023-04-20 12:06:43 -0300

    feat(exercise): added exercise 3

---
+++ exercise3.php
@@ -0,0 +1,13 @@
+<?php
+    function echoln(string $message) {
+        echo $message . "<br/>";
+    }
+
+    function calcHypot(float $cat1, float $cat2) {
+        return sqrt($cat1) + sqrt($cat2);
+    }
+
+    echoln(calcHypot(25.4, 22.1));
+    echoln(calcHypot(15.4, 2.1));
+    echoln(calcHypot(66, 5.5));
+    echoln(calcHypot(1, 5));
