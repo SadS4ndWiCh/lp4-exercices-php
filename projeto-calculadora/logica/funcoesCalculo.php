f7f8567f288042df77037e088eb1a158d2a5a446
SadS4ndWiCh <caiocamargo007@gmail.com>
2023-04-27 12:16:17 -0300

    feat: added temperature convertion functions

--- projeto-calculadora/logica/funcoesCalculo.php
+++ projeto-calculadora/logica/funcoesCalculo.php
@@ -11,3 +11,10 @@
     function div(int $num1, int $num2) {
         return $num1 / $num2;
     };
+
+    function celciusToFahrenheit(float $celcius) {
+        return ($celcius * 9/5) + 32;
+    }
+    function fahrenheitToCelcius(float $fahrenheit) {
+        return ($fahrenheit - 32) * 5/9;
+    }
