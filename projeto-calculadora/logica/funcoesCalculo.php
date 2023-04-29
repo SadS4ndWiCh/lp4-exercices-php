35543a18639967255a05356e67895704d1002f4a
SadSAndWiCh _ <caiocamargo007@gmail.com>
2023-04-29 18:46:06 -0300

    feat(functions): added centimeters, meters and kilometers convertions

--- projeto-calculadora/logica/funcoesCalculo.php
+++ projeto-calculadora/logica/funcoesCalculo.php
@@ -18,3 +18,16 @@
     function fahrenheitToCelcius(float $fahrenheit) {
         return ($fahrenheit - 32) * 5/9;
     }
+
+    function centimetersToMeters(float $centimeters) {
+        return $centimeters / 100;
+    }
+    function metersToCentimeters(float $meters) {
+        return $meters * 100;
+    }
+    function metersToKilometers(float $meters) {
+        return $meters / 1000;
+    }
+    function kilemetersToMeters(float $kilometers) {
+        return $kilometers * 1000;
+    }
